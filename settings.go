4c6bbcee707f4da4478e1dd87f3737250b7bada1
TKMAX777 <service@tk-max.com>
2023-03-26 16:28:34 +0900

    refactor

--- settings.go
+++ settings.go
@@ -1,3 +1,4 @@
 package relative_input
 
+const PROTOCOL_VERSION = 1
 const CHANNEL_NAME = "RELINP"
--- windows/host/windows.go
+++ windows/host/windows.go
@@ -1,109 +1,23 @@
 //go:build windows
 // +build windows
 
-package relative_input
+package host
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"os"
-	"os/signal"
 	"runtime"
 	"strconv"
 	"strings"
-	"syscall"
 	"unsafe"
 
+	relative_input "github.com/TKMAX777/RDPRelativeInput"
 	"github.com/TKMAX777/RDPRelativeInput/debug"
 	"github.com/TKMAX777/RDPRelativeInput/keymap"
 	"github.com/TKMAX777/RDPRelativeInput/remote_send"
 	"github.com/TKMAX777/RDPRelativeInput/winapi"
-	"github.com/TKMAX777/RDPRelativeInput/windows_client/client"
 	"github.com/lxn/win"
 )
-
-const PROTOCOL_VERSION = 1
-
-func StartClient() {
-	defer os.Stderr.Write([]byte("CLOSE\n"))
-
-	debug.Debugln("==== START CLIENT APPLICATION ====")
-	debug.Debugln("ServerProtocolVersion:", PROTOCOL_VERSION)
-
-	debug.Debugf("Wait for client headers...")
-
-	var scanner = bufio.NewScanner(os.Stdin)
-
-	for {
-		scanner.Scan()
-		var line = scanner.Text()
-
-		if strings.HasSuffix(line, strconv.Itoa(PROTOCOL_VERSION)) {
-			break
-		}
-
-		debug.Debugln("error!")
-		debug.Debugln("Get: ", line)
-		debug.Debugln("SendStatus:INVALID_PROTOCOL_VERSION")
-
-		// send invalid protocol version response
-		fmt.Printf("Status:INVALID_PROTOCOL_VERSION\n")
-	}
-	debug.Debugln("ok")
-
-	// response
-	fmt.Printf("RDPRelativeInput\n")
-	fmt.Printf("Status:OK\n")
-
-	debug.Debugln("SendStatus:OK")
-
-	var rHandler = remote_send.New(os.Stdout)
-	var wHandler = client.New(rHandler)
-
-	var toggleKey = os.Getenv("RELATIVE_INPUT_TOGGLE_KEY")
-	if toggleKey == "" {
-		toggleKey = "F8"
-	}
-
-	var toggleType = os.Getenv("RELATIVE_INPUT_TOGGLE_TYPE")
-	switch toggleType {
-	case "ONCE":
-		wHandler.SetToggleType(client.ToggleTypeOnce)
-	default:
-		wHandler.SetToggleType(client.ToggleTypeAlive)
-	}
-
-	wHandler.SetToggleKey(toggleKey)
-
-	var rdHwnd win.HWND
-	for {
-		rdHwnd = winapi.FindWindowEx(0, rdHwnd, winapi.MustUTF16PtrFromString("TscShellContainerClass"), nil)
-		if rdHwnd == 0 {
-			win.MessageBox(0, winapi.MustUTF16PtrFromString("Could not find window"), winapi.MustUTF16PtrFromString("RDP Relative Input"), win.MB_ICONERROR)
-			debug.Debugln("Window not found error")
-			return
-		}
-		var name = winapi.GetWindowTextString(rdHwnd)
-		if strings.Contains(name, os.Getenv("SERVER_NAME")) {
-			debug.Debugln("Client window found: ", name)
-			break
-		}
-	}
-
-	_, err := wHandler.StartClient(rdHwnd)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Fprintln(os.Stderr, "Ready for sending messages")
-
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	wHandler.Close()
-}
 
 func StartServer() {
 	runtime.LockOSThread()
@@ -114,7 +28,7 @@ func StartServer() {
 	debug.Debugln("==== LOGGING START ====")
 	debug.Debugf("OpenVirtualChannel...")
 
-	rw, err := winapi.OpenWTSVirtualChannel(winapi.WTS_CURRENT_SESSION, CHANNEL_NAME, 0)
+	rw, err := winapi.OpenWTSVirtualChannel(winapi.WTS_CURRENT_SESSION, relative_input.CHANNEL_NAME, 0)
 	if err != nil {
 		debug.Debugln("error!")
 		debug.Debugln(err)
@@ -146,7 +60,7 @@ func StartServer() {
 	debug.Debugf("SendHeaders...")
 
 	fmt.Fprintln(rw, "RDPRelativeInput")
-	fmt.Fprintf(rw, "ProtocolVersion:%d\n", PROTOCOL_VERSION)
+	fmt.Fprintf(rw, "ProtocolVersion:%d\n", relative_input.PROTOCOL_VERSION)
 
 	debug.Debugln("ok")
 
